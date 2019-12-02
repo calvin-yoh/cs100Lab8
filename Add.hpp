58cd3de017edd7da6e61be120017a1c192fbf6b3
Calvin Yoh <flashfire340@gmail.com>
2019-12-02 14:32:16 -0800

    Added Iterator function to Base and Add

--- Add.hpp
+++ Add.hpp
@@ -7,15 +7,21 @@
 class Add : public Base {
     public:
         Add(Base* value1,Base* value2) : Base()
- {
-        this->left = value1;
-        this->right = value2;
- }
+ 	{
+        	this->left = value1;
+        	this->right = value2;
+ 	}
         double evaluate() { return this->left->evaluate() +  this->right->evaluate();}
         std::string stringify()
         {
                 return this->left->stringify() + " + " + this->right->stringify();
         }
+
+	Iterator* create_iterator()
+	{
+		BinaryIterator* binIt = new BinaryIterator(this);
+		return binIt;	
+	}
 };
 
 #endif //__ADD_HPP__
--- base.hpp
+++ base.hpp
@@ -15,6 +15,7 @@ class Base {
         /* Pure Virtual Functions */
         virtual double evaluate() = 0;
         virtual std::string stringify() = 0;
+	virtual Iterator* create_iterator() = 0;
 };
 
 #endif //__BASE_HPP__
